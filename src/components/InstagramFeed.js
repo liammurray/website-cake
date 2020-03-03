ffd3ed675678d96b2f64ddd81997daafb5ec7030
Liam Murray <liammurray@users.noreply.github.com>
2020-03-02 17:48:59 -0800

    Add instagram and uploadcare tokens

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -8,7 +8,7 @@ import './InstagramFeed.css'
 
 export default class InstagramFeed extends Component {
   static defaultProps = {
-    accessToken: '1353697840.1677ed0.5a1cbfbc18f84915aa0d9a0bd02bff5a',
+    accessToken: '1539953711.452bff1.faa1f93aa00d4fdb938353e8be1dce2b',
     count: 20
   }
 
