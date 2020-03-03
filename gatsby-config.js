4faaad7992f71db7e02bac326084cf82e636d010
Liam Murray <liammurray@users.noreply.github.com>
2020-03-02 19:44:52 -0800

    A few tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
+    title: 'Liam Murray',
     siteUrl: 'https://liam.nod15c.com'
   },
   plugins: [
